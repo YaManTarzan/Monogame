aebe047430c5b62fb7e252dd4945c31fdc0caa81
Hunter <hmp5049@gmail.com>
2022-09-11 12:07:51 -0700

    Add project files.

---
+++ Monogame/src/Ball.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame
+{ 
+    public class Ball : Game
+    {
+        private Texture2D ballTexture;
+        private double _scale;
+        private SpriteBatch _spriteBatch;
+
+        Vector2 ballPosition;
+        float ballSpeed;
+        public float velX;
+        public float velY;
+
+        public Ball(Vector2 position, double scale, float speed, SpriteBatch batch, Texture2D texture)
+        {
+            ballPosition = position;
+            _scale = scale;
+            ballSpeed = speed;
+            _spriteBatch = batch;
+
+            ballTexture = texture;
+
+            velX = 0;
+            velY = 0;
+        }
+
+        public void Draw()
+        {
+            this.Update();
+
+            _spriteBatch.Draw(
+                ballTexture,
+                ballPosition,
+                null,
+                Color.White,
+                0f,
+                new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
+                Vector2.One,
+                SpriteEffects.None,
+                0f);
+        }
+
+        private void Update()
+        {
+            ballPosition.X += velX * ballSpeed;
+            ballPosition.Y += velY * ballSpeed;
+        }
+
+        public Vector2 GetPos()
+        {
+            return ballPosition;
+        }
+        
+        public void SetSpeed(float speed)
+        {
+            ballSpeed = speed;
+        }
+
+        public Vector2 GetSize()
+        {
+            return new Vector2(ballTexture.Width, ballTexture.Height);
+        }
+    }
+}
---
+++ Monogame/src/Game1.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame
+{
+    public class Game1 : Game
+    {
+        Texture2D ballTexture;
+        public Ball ball;
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public int WIDTH;
+        public int HEIGHT;
+
+        public Game1(int width = 1080, int height = 720)
+        {
+            WIDTH = width;
+            HEIGHT = height;
+
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferHeight = HEIGHT;
+            _graphics.PreferredBackBufferWidth = WIDTH;
+            
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Vector2 ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
+            ballTexture = Content.Load<Texture2D>("ball");
+            ball = new Ball(ballPosition, 1, 100f, _spriteBatch, ballTexture);
+            ball.velX = 1f;
+            ball.velY = 1f;
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            var kstate = Keyboard.GetState();
+
+            if (ball.GetPos().X < 0 + ball.GetSize().X / 2)
+                ball.velX *= -1f;
+            if (ball.GetPos().X > WIDTH - ball.GetSize().X / 2)
+                ball.velX *= -1f;
+            if (ball.GetPos().Y < 0 + ball.GetSize().Y / 2)
+                ball.velY *= -1;
+            if (ball.GetPos().Y > HEIGHT - ball.GetSize().Y / 2)
+                ball.velY *= -1;
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            ball.Draw();
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
